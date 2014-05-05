f1ea90484b25ab6231c58acd97f074222d5d90f0
martingruhn <mail@mgruhn.de>
2014-05-05 09:35:07 +0200

    setup basic maven projects; adds base classes from the Lambda section of Oracle's java tutorial

---
+++ src/main/java/de/is24/dojo/java8/Person.java
@@ -0,0 +1,11 @@
+package de.is24.dojo.java8;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mgruhn
+ * Date: 5/5/14
+ * Time: 9:09 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Person {
+}
